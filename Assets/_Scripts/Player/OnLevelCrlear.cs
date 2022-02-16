7ad37ee08f7222faee6a055df890ef2b12c1a009
aabusha3 <aabusha3@uwo.ca>
2022-02-16 15:43:32 -0500

    code clean up

--- Assets/_Scripts/Player/OnLevelCrlear.cs
+++ Assets/_Scripts/Player/OnLevelCrlear.cs
@@ -1,23 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AllEnemiesAreDead : MonoBehaviour
+public class OnLevelCrlear : MonoBehaviour
 {
     GameObject enemies;//the gameobject that all enemies spawn under
+
+    // Start is called before the first frame update
     void Start()
     {
         enemies = GameObject.Find("Enemies");//finds the gameobject
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (enemies.transform.childCount != 0)//if enemies are still alive
             return;
-        
-        if (SceneManager.GetActiveScene().name == "Level 1")//when player has killed all the enemies in Level 1
+
+        if (SceneManager.GetActiveScene().name == "Level 1")//when player has killed all the enemies in Level 1 load ArenaSetup
             SceneManager.LoadScene("ArenaSetup");
 
-        if (SceneManager.GetActiveScene().name == "ArenaSetup")//when player has killed all the enemies in ArenaSetup
+        if (SceneManager.GetActiveScene().name == "ArenaSetup")//when player has killed all the enemies in ArenaSetup load Level 1
             SceneManager.LoadScene("Level 1");
     }
 }
