293564799ae8ada28c1e80e2e35d60116dd00e3b
LucasHartmanWestern <lhartma8@uwo.ca>
2022-03-16 14:59:53 -0400

    Rotate player to where they are locked on before attacking

--- Assets/__Scripts/Combat/PlayerAttack.cs
+++ Assets/__Scripts/Combat/PlayerAttack.cs
@@ -5,6 +5,7 @@ public class PlayerAttack : MonoBehaviour
     InputManager inputManager; // Reference to InputManager class
     PlayerGeneral playerGeneral; // Reference to the PlayerGeneral class
     PlayerAnimationManager playerAnimationManager; // Reference to the PlayerAnimationManager class
+    CameraManager cameraManager; // Reference to the CameraManager class
     [SerializeField] private LayerMask _aimColliderMask; // All layers that player can aim at
     [SerializeField] AudioSource shootSoundEffect; // Get the audio source attached to the player that contains the sound effect for shooting
     [SerializeField] AudioSource swingSoundEffect; // Get the audio source attached to the player that contains the sound effect for swinging their melee weapon
@@ -23,6 +24,7 @@ public class PlayerAttack : MonoBehaviour
         inputManager = FindObjectOfType<InputManager>(); // Get reference to instance of InputManager
         playerGeneral = GetComponent<PlayerGeneral>(); // Get refernce to instance of PlayerGeneral attached to player
         playerAnimationManager = GetComponent<PlayerAnimationManager>(); // Get reference to instance of PlayerAnimationManager
+        cameraManager = FindObjectOfType<CameraManager>(); // Get reference to the Camera Manager instance
     }
 
     // Called after Awake()
@@ -89,6 +91,21 @@ public class PlayerAttack : MonoBehaviour
         // Make sure player is not on the ground and not interacting
         if (inputManager.attackInput)
         {
+            // If player is locked onto a target, rotate them towards that target before attacking
+            if (cameraManager.currentLockOnTarget != null)
+            {
+                Vector3 lockOnTarget = cameraManager.currentLockOnTarget.position; // Set the lockOnTarget
+                Vector3 attackDirection = (lockOnTarget - transform.position).normalized; // Determine where the user is trying to attack relative to the player
+
+                attackDirection.Normalize(); // Set magnitude to 1
+                attackDirection.y = 0; // Set value on y axis to 0
+
+                Quaternion targetRotation = Quaternion.LookRotation(attackDirection); // Look towards the target direction defined above
+                Quaternion playerRotation = targetRotation; // Get quaternion of the player's roation
+
+                transform.rotation = playerRotation; // Rotate the transform of the player
+            }
+
             playerAnimationManager.PlayTargetAnimation("Sword Swing", true); // Call the landing animation and don't allow the player to break out of it
             swingSoundEffect.Play(); // Play the sound effect for a swing
             inputManager.attackInput = false; // Make it so player must press the attack button each time
