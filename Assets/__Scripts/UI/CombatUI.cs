c648f5bfa43e5fb25be56ea8133f8f5d31542bd8
andrewcati <90794032+andrewcati@users.noreply.github.com>
2022-03-19 20:50:40 -0400

    Add files via upload
    Scripts for Scenes + buttons

--- Assets/__Scripts/UI/CombatUI.cs
+++ Assets/__Scripts/UI/CombatUI.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,7 +7,6 @@ public class CombatUI : MonoBehaviour
 {
     InputManager inputManager; // Reference to InputManager
     [SerializeField] private Image _crosshair;
-    [SerializeField] private GameObject _lockOnIndicator;
 
     // Awake is called before Start()
     private void Awake()
@@ -17,6 +18,5 @@ public class CombatUI : MonoBehaviour
     void Update()
     {
         _crosshair.enabled = inputManager.aimInput; // Only enable the crosshair if the player is aiming
-        _lockOnIndicator.SetActive(inputManager.lockOnFlag); // Only enable the lock on input if player is locked on
     }
 }
---
+++ Assets/__Scripts/UI/Selector_Script.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector_Script : MonoBehaviour
+{
+   public GameObject Gunner;
+   public GameObject Snips;
+   public GameObject Book;
+   private Vector3 CharacterPosition;
+   private Vector3 OffScreen;
+   private int CharacterInt = 1;
+   private SpriteRenderer GunnerRender, SnipsRender, BookRender;
+
+   private void Awake()
+   {
+      CharacterPosition = Gunner.transform.position;
+      OffScreen = Snips.transform.position;
+      GunnerRender = Gunner.GetComponent<SpriteRenderer>();
+      SnipsRender = Gunner.GetComponent<SpriteRenderer>();
+      BookRender = Gunner.GetComponent<SpriteRenderer>();
+   }
+
+   public void NextCharacter()
+   {
+         switch(CharacterInt)
+         {
+            case 1:
+               GunnerRender.enabled = false;
+               Gunner.transform.position = OffScreen;
+               Snips.transform.position = CharacterPosition;
+               SnipsRender.enabled = true;
+               CharacterInt++;
+               break;
+            case 2:
+               SnipsRender.enabled = false;
+               Snips.transform.position = OffScreen;
+               Book.transform.position = CharacterPosition;
+               BookRender.enabled = true;
+               CharacterInt++;
+               break;
+            case 3:
+               BookRender.enabled = false;
+               Book.transform.position = OffScreen;
+               Gunner.transform.position = CharacterPosition;
+               GunnerRender.enabled = true;
+               CharacterInt++;
+               ResetInt();
+               break;
+            default:
+               ResetInt();
+               break;         
+
+         }
+   }
+
+   public void PreviousCharacter()
+   {
+      switch(CharacterInt)
+         {
+            case 1:
+               GunnerRender.enabled = false;
+               Gunner.transform.position = OffScreen;
+               Book.transform.position = CharacterPosition;
+               BookRender.enabled = true;
+               CharacterInt--;
+               ResetInt();
+               break;
+            case 2:
+               SnipsRender.enabled = false;
+               Snips.transform.position = OffScreen;
+               Gunner.transform.position = CharacterPosition;
+               GunnerRender.enabled = true;
+               CharacterInt--;
+               break;
+            case 3:
+               BookRender.enabled = false;
+               Book.transform.position = OffScreen;
+               Snips.transform.position = CharacterPosition;
+               SnipsRender.enabled = true;
+               CharacterInt--;
+               break;
+            default:
+               ResetInt();
+               break;         
+
+         }
+   }
+
+   private void ResetInt()
+   {
+      if(CharacterInt >= 3 )
+      {
+         CharacterInt = 1;
+      }
+      else
+      {
+         CharacterInt = 3;
+      }
+   }
+
+}
