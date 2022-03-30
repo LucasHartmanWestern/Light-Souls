7efbe27fb96f9e185f1969bdbdbc309d1916cf93
LucasHartmanWestern <lhartma8@uwo.ca>
2022-03-29 21:53:58 -0400

    Add sniper and zoom effect for battle bot

--- Assets/__Scripts/Player/ClassSpecificScripts/BattleBot.cs
+++ Assets/__Scripts/Player/ClassSpecificScripts/BattleBot.cs
@@ -17,5 +17,31 @@ public class BattleBot : PlayerGeneral
     }
 
     // Handles the special combat ability of the BattleBot
-    protected override void HandleCombatAbility() { }
+    protected override void HandleCombatAbility() 
+    {
+        // Check if player is aiming and using their special ability input
+        if (inputManager.aimInput && inputManager.specialAbilityInput)
+        {
+            if (Camera.main.fieldOfView == 60) // Check if they are not in scope vision
+            {
+                Camera.main.fieldOfView = 20; // Zoom in the FOV
+                FindObjectOfType<CombatUI>().sniperCrosshair.SetActive(true); // Toggle sniper crosshair
+            }
+            else // The player is already in scope vision
+            {
+                Camera.main.fieldOfView = 60; // Zoom out the FOV
+                FindObjectOfType<CombatUI>().sniperCrosshair.SetActive(false); // Toggle sniper crosshair
+            }
+                
+
+            inputManager.specialAbilityInput = false; // Set to false
+
+        }
+        // If player isn't aiming then set everything back to normal
+        if (!inputManager.aimInput)
+        {
+            Camera.main.fieldOfView = 60;
+            FindObjectOfType<CombatUI>().sniperCrosshair.SetActive(false); // Make sure sniper crosshair is disabled
+        }
+    }
 }
--- Assets/__Scripts/UI/CombatUI.cs
+++ Assets/__Scripts/UI/CombatUI.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 public class CombatUI : MonoBehaviour
 {
     InputManager inputManager; // Reference to InputManager
-    [SerializeField] private Image _crosshair;
-    [SerializeField] private GameObject _lockOnIndicator;
+    [SerializeField] private GameObject _lockOnIndicator; // Reference to the lock on indicator
+    [SerializeField] private Image _crosshair; // Reference to crosshair image
+    public GameObject sniperCrosshair; // Reference to the sniper crosshair player 3 uses
 
     // Start is called after Start()
     private void Start()
@@ -18,7 +19,11 @@ public class CombatUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _crosshair.enabled = inputManager.aimInput; // Only enable the crosshair if the player is aiming
+        if (sniperCrosshair.activeSelf == false) // Check if sniper crosshair is enabled
+            _crosshair.enabled = inputManager.aimInput; // Only enable the crosshair if the player is aiming
+        else
+            _crosshair.enabled = false; // Disable crosshair if sniper crosshair is displayed
+
         _lockOnIndicator.SetActive(inputManager.lockOnFlag); // Only enable the lock on input if player is locked on
     }
 }
