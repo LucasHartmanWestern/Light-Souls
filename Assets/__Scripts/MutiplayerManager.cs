206e014fa6778dac935a8e132a9510caa800d24c
LucasHartman <lucas.hartman@kognitive.ca>
2023-04-13 10:20:07 -0400

    Revert "Assign userName to playerObject name"
    This reverts commit 9b90e3e17e50528115f2cd72f50caba41c6f5238.

--- Assets/__Scripts/MutiplayerManager.cs
+++ Assets/__Scripts/MutiplayerManager.cs
@@ -9,11 +9,13 @@ using System.Linq;
 
 public class MutiplayerManager : MonoBehaviour
 {
-    //string ipAddress = "54.196.231.67";
-   //int port = 2001;
+    string ipAddress = "54.196.231.67";
+    int port = 2001;
 
-    string ipAddress = "127.0.0.1";
-    int port = 3000;
+    public string serverName = "Test Server 1";
+
+    /*string ipAddress = "127.0.0.1";
+    int port = 3000;*/
 
     public Transform playerTransform;
 
@@ -140,9 +142,8 @@ public class MutiplayerManager : MonoBehaviour
 
     private void Update()
     {
-        playerTransform.gameObject.name = userName;//sets gameobject name to username
         string[] values = {
-            userName, ipAddress, port.ToString(),
+            userName, serverName, ipAddress, port.ToString(),
             playerTransform.position.x.ToString(), playerTransform.position.y.ToString(), playerTransform.position.z.ToString(),
             playerTransform.rotation.eulerAngles.x.ToString(), playerTransform.rotation.eulerAngles.y.ToString(), playerTransform.rotation.eulerAngles.z.ToString(),
             bigMag.ToString(), gas.ToString(), rocketBoots.ToString(), hiCalBullets.ToString(), energyDrink.ToString(), specialSerum.ToString(), bodyArmor.ToString(), aimChip.ToString(), loCalBullet.ToString(), fourLeaf.ToString(),
@@ -171,8 +172,6 @@ public class MutiplayerManager : MonoBehaviour
             gameObject.GetComponent<InputManager>().specialAbilityInput.ToString(),
             gameObject.GetComponent<InputManager>().reloadInput.ToString()
         };
-
-        
 
         message = string.Join(",", values);
 
@@ -205,6 +204,8 @@ public class MutiplayerManager : MonoBehaviour
         Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         socket.Connect(ipAddress, port);
 
+        Debug.Log(ipAddress + " | " + port);
+
         while (true)
         {
             if (finalData?.Length < 1) continue;
@@ -225,8 +226,11 @@ public class MutiplayerManager : MonoBehaviour
             {
                 string serverName = splitResponse[i].Split(',')[0];
                 string hostName = message.Split(',')[0];
-                
-                if (serverName == hostName)
+
+                string serverGameName = splitResponse[i].Split(',')[1];
+                string hostGameName = message.Split(',')[1];
+
+                if (serverName == hostName || serverGameName != hostGameName)
                 {
                     splitResponse[i] = null;
                 }
